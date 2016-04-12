5d91d21842de0e7994dd1e965f7ab0dc40026c17
Steffen Gebert <steffen.gebert@typo3.org>
2016-04-12 12:07:02 +0200

    [TASK] Simplify package installation code

--- cookbooks/site_t3chefjenkins_dev/recipes/_packages.rb
+++ cookbooks/site_t3chefjenkins_dev/recipes/_packages.rb
@@ -4,10 +4,4 @@ Installs basic packages on the machine
 #>
 =end
 
-# TODO as of 2016-03-20 this installs Vagrant 1.6.5
-# TODO find out how to install a more recent version on Debian
-%w(git vim).each do | package_name |
-	package package_name do
-		action :install
-	end
-end
+package %w(git vim)
