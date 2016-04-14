f027cb26f96677eb622faa33bbd13ca6be0b7635
Steffen Gebert <steffen.gebert@typo3.org>
2016-04-14 13:58:04 +0200

    [FEATURE] Inject workstation's chef credentials into test-kitchen VM
    As we are testing our CI pipeline, we need access to the Chef server at
    some points (berks resolution, cookbook upload).
    To avoid creation of test/dummy users, we can inject the credentials of
    the dude running `kitchen converge` into the VM so that it has full horse
    power.
    
    This can be controlled in the .kitchen.yml, optionally extended / disabled
    by default.

--- cookbooks/site_t3chefjenkins_dev/recipes/_chefdk.rb
+++ cookbooks/site_t3chefjenkins_dev/recipes/_chefdk.rb
@@ -15,3 +15,26 @@ if node['t3chefjenkins']['use_docker']
     end
   end
 end
+
+directory "#{node['jenkins']['master']['home']}/.chef/" do
+  owner "jenkins"
+  group "jenkins"
+end
+
+template "#{node['jenkins']['master']['home']}/.chef/config.rb" do
+  owner "jenkins"
+  group "jenkins"
+  source "chef-config.rb.erb"
+  variables(
+    :node_name => node['t3chefjenkins']['knife_config']['node_name'],
+    :chef_server_url => node['t3chefjenkins']['knife_config']['chef_server_url'],
+  )
+end
+
+file "#{node['jenkins']['master']['home']}/.chef/client.pem" do
+  owner "jenkins"
+  group "jenkins"
+  content node['t3chefjenkins']['knife_client_key'].gsub(/\|/, "\n") if node['t3chefjenkins']['knife_client_key']
+  content "Manually replace this with the real client.pem!" unless node['t3chefjenkins']['knife_client_key']
+  action :create unless node['t3chefjenkins']['knife_client_key']
+end
