1b9fb60891f4649f1a17d543678b198077a05671
Steffen Gebert <steffen.gebert@typo3.org>
2016-05-04 19:56:22 +0200

    Restart jenkins after adding to docker group

--- cookbooks/site_t3chefjenkins_dev/recipes/_docker.rb
+++ cookbooks/site_t3chefjenkins_dev/recipes/_docker.rb
@@ -3,10 +3,6 @@ return unless node['t3chefjenkins']['use_docker']
 include_recipe "apt-docker"
 package "docker-engine"
 
-# service "docker" do
-#   supports [:restart]
-# end
-
 # add jenkins to the docker group, so that it doesn't need to use
 # sudo.  Alternatively, we could configure sudo such that jenkins can
 # run "docker" without a password. / compare the .kitchen.docker.yml that we write.
@@ -14,5 +10,5 @@ group "docker" do
   members "jenkins"
   append true
   action :modify
-  notifies :execute, "jenkins_command[safe-restart]"
+  notifies :restart, "service[jenkins]"
 end
