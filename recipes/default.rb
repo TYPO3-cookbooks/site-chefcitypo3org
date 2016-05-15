c07e8a4bc171b8e597f1bbc5883e97a83bed6ed9
Steffen Gebert <steffen.gebert@typo3.org>
2016-05-15 21:33:19 +0200

    Do not include chef-zero cookbook - not needed

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,5 +13,3 @@ end
 %w(_packages _jenkins_setup _chefdk _jenkins_plugins _jenkins_auth _jenkins_jobs _docker _misc).each do | local_recipe |
   include_recipe "site-chefcitypo3org::#{local_recipe}"
 end
-
-include_recipe 'chef-zero'
