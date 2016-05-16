22ff30dc3b8015594aedf41f3f483bd31576934b
Steffen Gebert <steffen.gebert@typo3.org>
2016-05-16 15:01:11 +0200

    Revert "Update to ChefDK 0.13.21"
    This reverts commit afb2676411e6034e4b3ddb1725c344c0e6626d10.
    
    `rubocop` isn't in $PATH anymore.
    
    Upstream issue: chef/chef-dk#809

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,7 +17,7 @@ default['jenkins']['master']['shell'] = '/bin/bash'
 default['jenkins']['master']['plugins'] = {}
 
 #<> Configures the ChefDK version to be installed - see https://github.com/chef/chef-dk
-default['site-chefcitypo3org']['chefdk']['version'] = '0.13.21-1'
+default['site-chefcitypo3org']['chefdk']['version'] = '0.12.0-1'
 
 #<> Configures the version of Chef to use for test-kitchen runs
 default['site-chefcitypo3org']['kitchen']['chef_version'] = "12.5.1"
