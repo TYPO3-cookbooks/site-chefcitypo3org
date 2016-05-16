277d5e2452fc8457222d2992351e9f5d0d6f3f84
Steffen Gebert <steffen.gebert@typo3.org>
2016-05-16 15:11:44 +0200

    Bump version to 0.1.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mimi@kaktusteam.def'
 license          'All rights reserved'
 description      'Provisions a Jenkins master server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
+version          '0.1.6'
 
 supports         'debian'
 
