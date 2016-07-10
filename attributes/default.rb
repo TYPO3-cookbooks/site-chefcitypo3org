cdcaac8ff704f0131500ffe1236f1f6ccfa75264
Steffen Gebert <steffen.gebert@typo3.org>
2016-07-10 14:00:26 +0200

    [TASK] Upgrade ChefDK to 0.15.16

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@
 #################
 
 #<> Configures the ChefDK version to be installed - see https://github.com/chef/chef-dk
-default['site-chefcitypo3org']['chefdk']['version'] = "0.12.0-1"
+default['site-chefcitypo3org']['chefdk']['version'] = "0.15.16-1"
 
 #<> Configures the version of Chef to use for test-kitchen runs
 default['site-chefcitypo3org']['kitchen']['chef_version'] = "12.5.1"
