63f981ba0cb547fcef1a07dd29a227263bbbe1ef
Steffen Gebert <steffen.gebert@typo3.org>
2016-07-15 12:25:12 +0200

    [TESTS] Add data bag based password TK suite

--- test/integration/default/inspec/jenkins_spec.rb
+++ test/integration/default/inspec/jenkins_spec.rb
@@ -1,8 +1,6 @@
 control 'jenkins-1' do
   title 'Jenkins Setup'
-  desc '
-    Check that jenkins is installed and listening to ports
-  '
+  desc 'Check that jenkins is installed and listening to ports'
   describe package('jenkins') do
     it { should be_installed }
   end
@@ -17,4 +15,21 @@ control 'jenkins-1' do
     its('protocols') { should include 'tcp6'}
     # its('processes') { should include 'java' }
   end
+
 end
+
+
+control 'jenkins-2' do
+  title 'Jenkins No-Auth'
+  desc 'Check that Jenkins has no auth configured'
+
+  xml_parse_options = {
+    assignment_re: /^\s*<([^>]*?)>(.*?)<\/[^>]*>\s*$/,
+  }
+
+  describe parse_config_file('/var/lib/jenkins/config.xml', xml_parse_options) do
+    its('useSecurity') { should eq 'true' }
+  end
+
+end
+
