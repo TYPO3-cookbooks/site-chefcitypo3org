2fe125670d996535cab506df0b7cb2fe4fff3d79
Steffen Gebert <steffen.gebert@typo3.org>
2016-07-21 00:31:44 +0200

    [TASK] Update to jenkins=2.6.0

--- metadata.rb
+++ metadata.rb
@@ -13,7 +13,7 @@ depends          't3-chef-vault', '~> 1.0.0'
 
 # Make sure to freeze all versions of community cookbooks here!
 depends          'java',              '= 1.39.0'
-depends          'jenkins',           '= 2.5.0'
+depends          'jenkins',           '= 2.6.0'
 depends          'ssh_known_hosts',   '= 2.0.0'
 depends          'chef-dk',           '= 3.1.0'
 
