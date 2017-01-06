161c330817fdbd1a096916f34b262c3789c90b25
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-06 09:24:46 +0100

    [TASK] Bump version to 1.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adminATtypo3DOTorg'
 license          'Apache 2.0'
 description      'Provisions a Chef CI/CD server based on Jenkins.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.2'
+version          '1.1.3'
 
 supports         'debian'
 
