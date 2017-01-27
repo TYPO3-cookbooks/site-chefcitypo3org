81626536d513fa4bbb77e86dc96911e11f6fc97b
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-27 20:17:59 +0100

    [TASK] Use up-to-date dependencies

--- metadata.rb
+++ metadata.rb
@@ -13,8 +13,8 @@ depends          't3-chef-vault', '~> 1.0.0'
 depends          'jenkins-chefci'# , '~> 1.0.0'
 
 # Make sure to freeze all versions of community cookbooks here!
-depends          'java',              '= 1.39.0'
-depends          'jenkins',           '= 3.0.0'
+depends          'java',              '= 1.46.0'
+depends          'jenkins',           '= 4.2.1'
 depends          'ssh_known_hosts',   '= 2.0.0'
 depends          'chef-dk',           '= 3.1.0'
 
