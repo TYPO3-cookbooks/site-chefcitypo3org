6f09616e0c784c275373512320456895a3b1a3ce
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-28 19:06:58 +0100

    [CLEANUP] Docker installation provided by jenkins-chefci

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,6 +12,6 @@ end
 
 include_recipe 'jenkins-chefci::full'
 
-%w(_packages _jenkins_setup _chefdk _jenkins_plugins _jenkins_auth _jenkins_jobs _docker _ssh).each do |local_recipe|
+%w(_packages _jenkins_setup _chefdk _jenkins_plugins _jenkins_auth _jenkins_jobs _ssh).each do |local_recipe|
   include_recipe "site-chefcitypo3org::#{local_recipe}"
 end
