255893da4549c43a18af15c9bcf32361a270532d
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-28 19:06:02 +0100

    [TASK] rescue version to 0.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adminATtypo3DOTorg'
 license          'Apache 2.0'
 description      'Provisions a Chef CI/CD server based on Jenkins.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 supports         'debian'
 
