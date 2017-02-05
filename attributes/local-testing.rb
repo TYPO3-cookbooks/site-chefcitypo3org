fd5c08b75aaa78f63b651da04e45dbf721785f69
Steffen Gebert <steffen.gebert@typo3.org>
2017-02-05 15:23:39 +0100

    [TASK] Update to changes in jenkins-chefci

--- attributes/local-testing.rb
+++ attributes/local-testing.rb
@@ -1,19 +1,7 @@
 # These attributes are used by .kitchen.yml to ease local development of this cookbook
 
-#<> The knife/chef configuration for communicating with the Chef API
-default['site-chefcitypo3org']['knife_config'] = {}
-
-#<> Optionally (for local testing), the contents of a chef admin's key (\n replaced with |)
-default['site-chefcitypo3org']['knife_client_key'] = nil
-
 #<> Github OAuth client ID
 default['site-chefcitypo3org']['auth']['github_client_id'] = nil
 
 #<> Github OAuth client secret
 default['site-chefcitypo3org']['auth']['github_client_secret'] = nil
-
-#<> Github Username
-default['site-chefcitypo3org']['auth']['github_user'] = "chefcitypo3org"
-
-#<> Github Token
-default['site-chefcitypo3org']['auth']['github_token'] = nil
--- recipes/_chefdk.rb
+++ recipes/_chefdk.rb
@@ -6,35 +6,6 @@ ChefDK setup
 
 
 ###########################
-# Chef / Knife
-###########################
-
-directory "#{node['jenkins']['master']['home']}/.chef/" do
-  owner "jenkins"
-  group "jenkins"
-end
-
-template "#{node['jenkins']['master']['home']}/.chef/config.rb" do
-  owner "jenkins"
-  group "jenkins"
-  source "chef-config.rb.erb"
-  variables(
-    node_name: node['site-chefcitypo3org']['knife_config'].key?('node_name') ? node['site-chefcitypo3org']['knife_config']['node_name'] : "jenkins",
-    chef_server_url: node['site-chefcitypo3org']['knife_config']['chef_server_url'] || "https://chef.typo3.org"
-  )
-end
-
-# place either the client key defined in the attributes, or create only a template file
-file "#{node['jenkins']['master']['home']}/.chef/client.pem" do
-  owner "jenkins"
-  group "jenkins"
-  content node['site-chefcitypo3org']['knife_client_key'].tr("|", "\n") if node['site-chefcitypo3org']['knife_client_key']
-  action :create if node['site-chefcitypo3org']['knife_client_key']
-  content "Manually replace this with the real client.pem!" unless node['site-chefcitypo3org']['knife_client_key']
-  action :create_if_missing unless node['site-chefcitypo3org']['knife_client_key']
-end
-
-###########################
 # Test-Kitchen
 ###########################
 
