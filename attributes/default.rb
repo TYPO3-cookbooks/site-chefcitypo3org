4d12b9ae592959bcf5302d2b5168c8106d34289f
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-18 09:14:26 +0200

    [BUGFIX] Set Chef Server URL
    This got lost when moving the chefdk recipe to the jenkins-chefci
    cookbook.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,9 @@ default['site-chefcitypo3org']['url'] = "https://chef-ci.typo3.org"
 #<> URL of the main chef repo
 default['site-chefcitypo3org']['main_repo'] = "ssh://chef-jenkins@review.typo3.org:29418/Teams/Server/Chef.git"
 
+#<> URL of the CHef Server
+default['jenkins_chefci']['knife_config']['chef_server_url'] = 'https://chef.typo3.org'
+
 #<> Install Jenkins LTS
 default['jenkins']['master']['repository'] = "http://pkg.jenkins-ci.org/debian-stable"
 
