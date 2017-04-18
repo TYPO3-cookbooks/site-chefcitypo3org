48d41e21219c1c5d1be7d7303eefff331985277e
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-18 10:45:41 +0200

    [BUGFIX] Disable JobDSL Script Security
    The authorize-project plugin blocked any pipeline jobs from being
    executed. So disable security to keep the state as it was before
    Job DSL introduced that feature.

--- recipes/_jenkins_setup.rb
+++ recipes/_jenkins_setup.rb
@@ -23,37 +23,38 @@ end
 ##################################
 # Jenkins Job DSL Script Security
 ##################################
-# this would disable script security for the JobDSL plugin
+# this disables script security for the JobDSL plugin
 # see https://github.com/jenkinsci/job-dsl-plugin/wiki/Script-Security
-# jenkins_script 'jobdsl-script-security' do
-#   command <<-EOH.gsub(/^ {2}/, '')
-#   import javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration
-#
-#   Jenkins.getInstance().getDescriptorByType(GlobalJobDslSecurityConfiguration.class).useScriptSecurity = false
-#
-#   EOH
-# end
+jenkins_script 'jobdsl-script-security' do
+  command <<-EOH.gsub(/^ {2}/, '')
+  import javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration
+
+  Jenkins.getInstance().getDescriptorByType(GlobalJobDslSecurityConfiguration.class).useScriptSecurity = false
+
+  EOH
+end
 #
 # more secure: run as the user who triggered the build (using the authorize-project plugin)
 # see https://github.com/jenkinsci/job-dsl-plugin/wiki/Script-Security
-jenkins_script 'jobdsl-auth' do
-  command <<-EOH.gsub(/^ {4}/, '')
-    import jenkins.security.*
-    import hudson.util.DescribableList
-    import org.jenkinsci.plugins.authorizeproject.*
-    import org.jenkinsci.plugins.authorizeproject.strategy.*
-    
-    DescribableList<QueueItemAuthenticator, QueueItemAuthenticatorDescriptor> authenticators =
-      QueueItemAuthenticatorConfiguration.get().getAuthenticators();
-    
-    
-    if (authenticators.isEmpty()) {
-      println "Adding TriggeringUsersAuthorizationStrategy"
-      GlobalQueueItemAuthenticator triggerUserAuthenticator = new GlobalQueueItemAuthenticator(new TriggeringUsersAuthorizationStrategy())
-    
-      authenticators.add(triggerUserAuthenticator)
-    } else {
-      println "AuthenticationStrategy already set"
-    }
-  EOH
-end
+# somehow, this prevents other pipeline jobs to run
+# jenkins_script 'jobdsl-auth' do
+#   command <<-EOH.gsub(/^ {4}/, '')
+#     import jenkins.security.*
+#     import hudson.util.DescribableList
+#     import org.jenkinsci.plugins.authorizeproject.*
+#     import org.jenkinsci.plugins.authorizeproject.strategy.*
+#
+#     DescribableList<QueueItemAuthenticator, QueueItemAuthenticatorDescriptor> authenticators =
+#       QueueItemAuthenticatorConfiguration.get().getAuthenticators();
+#
+#
+#     if (authenticators.isEmpty()) {
+#       println "Adding TriggeringUsersAuthorizationStrategy"
+#       GlobalQueueItemAuthenticator triggerUserAuthenticator = new GlobalQueueItemAuthenticator(new TriggeringUsersAuthorizationStrategy())
+#
+#       authenticators.add(triggerUserAuthenticator)
+#     } else {
+#       println "AuthenticationStrategy already set"
+#     }
+#   EOH
+# end
