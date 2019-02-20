7c779881b5da99fae7274e7ecbc1c432fb80f520
Steffen Gebert <steffen.gebert@typo3.org>
2019-02-20 18:20:08 +0100

    [TASK] Update jenkins cookbook
    Remoting isn't supported anymore, thus we probably
    need a newer version

--- metadata.rb
+++ metadata.rb
@@ -17,7 +17,7 @@ depends          'jenkins-chefci', '~> 0.2.0'
 
 # Make sure to freeze all versions of community cookbooks here!
 depends          'java',              '= 1.50.0'
-depends          'jenkins',           '= 5.0.2'
+depends          'jenkins',           '= 6.2.1'
 depends          'ssh_known_hosts',   '= 2.0.0'
 depends          'chef-dk',           '= 3.1.0'
 
